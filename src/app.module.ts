2b6bc8aaefcf786fa3b72ba72064f94a39e1aa9d
Janck <sepulved.janck@gmail.com>
2026-03-28 13:28:20 -0400

    [FEAT]: add basis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './exchange/services/PrismaService';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaService
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService]
 })
 export class AppModule {}
