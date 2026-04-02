d58b8d3c7d976fe8a9f1628acdf6e932269bc125
Janck <sepulved.janck@gmail.com>
2026-04-02 12:52:32 -0400

    [FEAT]: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    app.setGlobalPrefix('api/v1');
     const config = new DocumentBuilder()
         .setTitle('Tu zona tienda API')
         .setDescription('API para la aplicación de tu zona tienda')
         .setVersion('1.0')
         .build();
     const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, documentFactory);
+    SwaggerModule.setup('api/v1/docs', app, documentFactory);
     await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
